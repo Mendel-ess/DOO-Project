891068dc8805f0a400f818b2fd4f035e3aa9849e
mardokeoo <101274889+mardokeoo@users.noreply.github.com>
2023-04-26 21:43:01 -0500

    Add files via upload

---
+++ backend/components/Home/container.js
@@ -0,0 +1,450 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment } from "react";
+import Siguenos from "../../img/fb-flv.webp";
+
+const container = () => {
+  return (
+    <Fragment>
+      <section class="container-principal">
+        <div class="container-body margin-container">
+          <aside class="sidebar MC__fixed">
+            <a href="">
+              <img src="img/fb-flv.webp" alt="siguenos" />
+            </a>{" "}
+            <div class="widget-emision">
+              <div class="title-emision fa-play">
+                <strong> ANIMES EN EMISIÓN </strong>{" "}
+              </div>{" "}
+              <div class="animes-emision">
+                <ul>
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                  <li>
+                    <a href="" class="fa-play-circle">
+                      Naruto Shippuden{" "}
+                      <span class="type-anime tv"> ANIME </span>{" "}
+                    </a>{" "}
+                  </li>{" "}
+                </ul>{" "}
+              </div>{" "}
+            </div>{" "}
+          </aside>{" "}
+          <main>
+            <div class="title">
+              <h2> Últimos episodios </h2>{" "}
+              <div class="order">
+                <a href="" class="active">
+                  {" "}
+                  HOY{" "}
+                </a>{" "}
+              </div>{" "}
+            </div>{" "}
+            <ul class="last-episodes">
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+              <li>
+                <a href="" class="fa-play">
+                  <span class="image-mask">
+                    <img src="img/miniatura.jpg" alt="miniatura" />
+                  </span>{" "}
+                  <span class="nCapi"> Episodio 1 </span>{" "}
+                  <strong class="title-cap-miniatura">
+                    Nomad: Megalo Box 2{" "}
+                  </strong>{" "}
+                </a>{" "}
+              </li>{" "}
+            </ul>{" "}
+            <div class="title fa-star">
+              <h2> Últimos Animes </h2>{" "}
+              <div class="order">
+                <a href="" class="active">
+                  {" "}
+                  HOY{" "}
+                </a>{" "}
+              </div>{" "}
+            </div>{" "}
+            <ul class="last-animes">
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+              <li>
+                <article class="anime">
+                  <span class="estreno">
+                    {" "}
+                    <span> ESTRENO </span>{" "}
+                  </span>{" "}
+                  <div class="image fa-play-circle-o">
+                    <figure>
+                      <img src="img/cover.jpg" alt="cover" />
+                    </figure>{" "}
+                    <span class="type-anime tv"> ANIME </span>{" "}
+                  </div>{" "}
+                  <h3 class="title-anime"> Fumetsu no Anata e </h3>{" "}
+                </article>{" "}
+              </li>{" "}
+            </ul>{" "}
+          </main>{" "}
+        </div>{" "}
+      </section>{" "}
+    </Fragment>
+  );
+};
+
+export default container;
